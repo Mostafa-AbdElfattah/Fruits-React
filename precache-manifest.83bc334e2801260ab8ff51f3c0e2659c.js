8a100a6d8dc48df12bf1c8b3b7ff4a7f66627fa6
TaAaAa7 <mostafa.ahmed.abdelfattah@gmail.com>
2020-04-29 01:29:44 +0000

    Updates

---
+++ precache-manifest.83bc334e2801260ab8ff51f3c0e2659c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd73d79088766e4210aefb88e1496f84",
+    "url": "/Fruits-React/index.html"
+  },
+  {
+    "revision": "8b21434c9780e9587631",
+    "url": "/Fruits-React/static/css/main.69372896.chunk.css"
+  },
+  {
+    "revision": "419b1afee6607b96b386",
+    "url": "/Fruits-React/static/js/2.0202c0ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Fruits-React/static/js/2.0202c0ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b21434c9780e9587631",
+    "url": "/Fruits-React/static/js/main.39d61d80.chunk.js"
+  },
+  {
+    "revision": "82d93a24899f98ed8f59",
+    "url": "/Fruits-React/static/js/runtime-main.0312321b.js"
+  },
+  {
+    "revision": "d0fc30856a541539009e73c787a68bc1",
+    "url": "/Fruits-React/static/media/Ubuntu-Bold.d0fc3085.ttf"
+  },
+  {
+    "revision": "b3488e8486d2b4e3a0666997f91c7ed9",
+    "url": "/Fruits-React/static/media/Ubuntu-Regular.b3488e84.ttf"
+  },
+  {
+    "revision": "c61bff1a0b230913d85175994281a114",
+    "url": "/Fruits-React/static/media/fruits-img.c61bff1a.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Fruits-React/precache-manifest.83bc334e2801260ab8ff51f3c0e2659c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Fruits-React/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
